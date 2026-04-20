52967366db963242c3d932ca079e46766fe5ed63
Silletr <silletrMain@gmail.com>
2026-04-20 09:55:51 +0300

    [CHANGED FILE/DIR: src/neovim-config-build/generate/theme.py | NEW FILE/DIR: src/neovim-config-build/generate/presets/*, src/neovim-config-build/ui/cli.py] [Type: Added preset's for future generating] Added template folder, will be used in future when I'll start writing a code [TIMESTAMP: 20/04/2026 | 9:55 UTC +2]

---
+++ src/neovim-config-build/generate/theme.py
@@ -0,0 +1,9 @@
+#    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+#    ┃                                  Will be used:                                  ┃
+#    ┃    1. os.environ (until I came up with something better than just env-var's)    ┃
+#    ┃    2. brain (But not guarranting)                                               ┃
+#    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+
+#  ─────────── Imports (Will be used in future generation) ────────────
+# import os
