ce37a238b24b7b60c22583303a111769324c3663
Silletr <silletrMain@gmail.com>
2026-04-22 14:24:28 +0300

    [CHANGED FILE/DIR: src/neovim-config-build/generate/theme.py] [Type: Added searching for repo by 'https://api.github.com/search/repositories?q={query}'] Well, guys, I almost here, need just few corrections, and I think I'll end with themes until Friday [TIMESTAMP: 22/04/2026 | 14:24 UTC+2]

--- src/neovim-config-build/generate/theme.py
+++ src/neovim-config-build/generate/theme.py
@@ -7,6 +7,7 @@
 
 #  ─────────────────────────── Imports (Will be used in future generation) ────────────────────────────
 from generate.logger import configure_logger
+import requests
 
 # Start only from ../ and with "python -m generate.theme"
 
@@ -21,8 +22,15 @@ return {
     vim.cmd("colorscheme " .. name)
     end,
 }
+
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    ┃    Repository search example    ┃
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+    https://api.github.com/search/repositories?q={query}
+
 """
 
+#  ────────────────────────────────────────────────────────────────────────────────────────────────────
 file_to_save: str = "theme"
 logger = configure_logger(file_to_save)
 theme_name, colorscheme = tuple(
@@ -30,5 +38,68 @@ theme_name, colorscheme = tuple(
     .strip()
     .split("|", 1)
 )
+
+if not theme_name or not colorscheme:
+    logger.error("Use the format! \nTheme | Colorscheme")
+
 logger.info(f"User's theme: {theme_name} | User's colorscheme {colorscheme}")
 print(f"Theme name is: {theme_name}, and a colorscheme is {colorscheme}!")
+
+
+#    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+#    ┃    Find a repo with limited value    ┃
+#    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+def format_repo_list(items, limit=3):
+    lines = []
+    for i, r in enumerate(items[:limit], start=1):
+        lines.append(f"{i}. {r['full_name']}")
+    return "\n".join(lines)
+
+
+#  ────────────────────────────────────────────────────────────────────────────────────────────────────
+# Search for Repo by User's theme name
+req = requests.get(f"https://api.github.com/search/repositories?q={theme_name}")
+if req.status_code != 200:
+    logger.error(f"Requests.get() got an error! Status code: {req.status_code}")
+
+data = req.json()
+items = data.get("items", [])
+logger.success(
+    f"""
+    Got a theme on Github! Found \n{len(items)} repo's,
+    The {items[0]["full_name"]} is the correct one!
+    """
+)
+
+if not items:
+    logger.error(f"No repositories found by: {theme_name}")
+    exit()
+
+repo = items[0]["full_name"]  # e.g EdenEast/nightfox.nvim
+logger.success(f"Found repo: {repo}")
+# especially for me, to remember indexes xD:
+# repo 1   repo 2   repo 3
+# 0        1         2
+
+if theme_name.lower() not in repo.lower():
+    logger.info(f"{theme_name} is different than {repo}, asking user to choose")
+    print(
+        f"Your {theme_name} is corresponding with other repo's - select needed"
+        "   (printed: first 3 repo):"
+    )
+    repo_list = format_repo_list(items, limit=10)
+
+    user_choose = int(
+        input(
+            f"""
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    ┃        Repository:         ┃
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+{repo_list}
+(Select with number:)
+>>> """
+        )
+    )
+    selected_repo = items[user_choose - 1]["full_name"]
+    logger.success(f"Found repo: {selected_repo}")
+    print(f"Got you, then I'll remember - {user_choose} is the correct one!")
